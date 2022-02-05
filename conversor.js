d2bb3f6d7eaebb7df256e208f5ea0709ac0f4187
Gabriel Amaro <gabriel000400@gmail.com>
2022-02-05 07:36:24 -0200

    Convercoin atualizado

--- conversor.js
+++ conversor.js
@@ -1,9 +1,9 @@
 const real = document.getElementById("value");
-const cotacaoDolar = 5.28;
+const cotacaoDolar = 5.33;
 const valorEmDolar = document.getElementById("output");
 
 function converter() {
-    const valorConvertido = real.value / cotacaoDolar;
+    const valorConvertido = (real.value / cotacaoDolar).toFixed(2);
 
-    valorEmDolar.innerHTML = "O valor em dólar americano é US$" + valorConvertido.toFixed(2);
+    valorEmDolar.innerHTML = "O valor em dólar americano é US$" + valorConvertido;
 }
